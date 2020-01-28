7a30eb70b6226f243d83359860b3c3c65f5ac158
JeanVanneste <jeanvanneste@gmail.com>
2020-01-28 17:08:51 +0100

    adding roles to creation user

--- src/Form/CreateUserType.php
+++ src/Form/CreateUserType.php
@@ -7,6 +7,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -17,8 +18,14 @@ class CreateUserType extends AbstractType
         $builder
             ->add('username', TextType::class)
             ->add('email', EmailType::class)
-            ->add('save', SubmitType::class)
-        ;
+            ->add('roles', ChoiceType::class, [
+                'choices' => [
+                    "KEJ" => "ROLE_KEJ",
+                ],
+                "expanded" => true,
+                "multiple" => true,
+            ])
+            ->add('save', SubmitType::class);
     }
 
     public function configureOptions(OptionsResolver $resolver)
