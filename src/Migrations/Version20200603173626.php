99a46070a9fcc75234929e7dceed5ce800d7221c
JeanVanneste <jeanvanneste@gmail.com>
2020-06-03 19:38:29 +0200

    cleaning migrations

--- src/Migrations/Version20200603173626.php
+++ src/Migrations/Version20200603173626.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20191223132208 extends AbstractMigration
+final class Version20200603173626 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -24,10 +24,11 @@ final class Version20191223132208 extends AbstractMigration
 
         $this->addSql('CREATE TABLE personne (id INT AUTO_INCREMENT NOT NULL, Nom VARCHAR(20) NOT NULL, Prenom VARCHAR(20) DEFAULT NULL, email TEXT DEFAULT NULL, kap VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE location (id INT AUTO_INCREMENT NOT NULL, personne_id INT NOT NULL, jeu_id INT NOT NULL, date_debut DATE NOT NULL, date_fin DATE NOT NULL, paye TINYINT(1) NOT NULL, INDEX IDX_5E9E89CBA21BD112 (personne_id), INDEX IDX_5E9E89CB8C9E392E (jeu_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, username VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, email VARCHAR(180) NOT NULL, connection_token VARCHAR(32) DEFAULT NULL, UNIQUE INDEX UNIQ_8D93D649F85E0677 (username), UNIQUE INDEX UNIQ_8D93D64999FD688E (connection_token), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE auteur (id INT AUTO_INCREMENT NOT NULL, nom VARCHAR(255) NOT NULL, prenom VARCHAR(255) DEFAULT NULL, nationalite VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE editeur (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, creation_year INT DEFAULT NULL, nationalite VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE editeur (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, creation_year INT DEFAULT NULL, nationalite VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE genre (id INT AUTO_INCREMENT NOT NULL, genre VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE jeu (id INT AUTO_INCREMENT NOT NULL, editeur_id INT DEFAULT NULL, titre VARCHAR(255) NOT NULL, description LONGTEXT DEFAULT NULL, joueurs_min INT NOT NULL, joueurs_max INT NOT NULL, duree INT NOT NULL, anne INT NOT NULL, annee INT NOT NULL, INDEX IDX_82E48DB53375BD21 (editeur_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE jeu (id INT AUTO_INCREMENT NOT NULL, editeur_id INT DEFAULT NULL, titre VARCHAR(255) NOT NULL, description LONGTEXT DEFAULT NULL, joueurs_min INT NOT NULL, joueurs_max INT NOT NULL, duree INT NOT NULL, annee INT NOT NULL, INDEX IDX_82E48DB53375BD21 (editeur_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE jeu_genre (jeu_id INT NOT NULL, genre_id INT NOT NULL, INDEX IDX_B1B530008C9E392E (jeu_id), INDEX IDX_B1B530004296D31F (genre_id), PRIMARY KEY(jeu_id, genre_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE jeu_auteur (jeu_id INT NOT NULL, auteur_id INT NOT NULL, INDEX IDX_B60E2E6D8C9E392E (jeu_id), INDEX IDX_B60E2E6D60BB6FE6 (auteur_id), PRIMARY KEY(jeu_id, auteur_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE location ADD CONSTRAINT FK_5E9E89CBA21BD112 FOREIGN KEY (personne_id) REFERENCES personne (id)');
@@ -53,6 +54,7 @@ final class Version20191223132208 extends AbstractMigration
         $this->addSql('ALTER TABLE jeu_auteur DROP FOREIGN KEY FK_B60E2E6D8C9E392E');
         $this->addSql('DROP TABLE personne');
         $this->addSql('DROP TABLE location');
+        $this->addSql('DROP TABLE user');
         $this->addSql('DROP TABLE auteur');
         $this->addSql('DROP TABLE editeur');
         $this->addSql('DROP TABLE genre');
