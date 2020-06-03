8b8be6d4e2cd3397cf03a635984f471fee268e6c
JeanVanneste <jeanvanneste@gmail.com>
2020-06-03 16:30:49 +0200

    Ajout confirmation to delete

--- public/script.js
+++ public/script.js
@@ -1,4 +1,4 @@
-function confirm() {
+function confirm_delete_jeu() {
     if (confirm("Supprimer le jeu ?\nAttention, cette action est définitive !")) {
         window.location.replace("{{ jeu.id }}/delete");
     }
