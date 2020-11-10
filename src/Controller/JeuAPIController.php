28923142c2be9b4704ab03d3330c36dca1c880a0
JeanVanneste <jeanvanneste@gmail.com>
2020-11-10 21:04:34 +0100

    Circular reference handling for api

--- src/Controller/JeuAPIController.php
+++ src/Controller/JeuAPIController.php
@@ -2,14 +2,14 @@
 
 namespace App\Controller;
 
+use App\Entity\Jeu;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\Encoder\JsonEncoder;
 use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
 use Symfony\Component\Serializer\Serializer;
-
-use App\Entity\Jeu;
+use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
 
 class JeuAPIController extends AbstractController
 {
@@ -18,9 +18,15 @@ class JeuAPIController extends AbstractController
      */
     public function index()
     {
-        $encoders = array(new JsonEncoder());
-        $normalizers = array(new ObjectNormalizer());
-        $serializer = new Serializer($normalizers, $encoders);
+        $defaultContext = [
+            AbstractNormalizer::CIRCULAR_REFERENCE_HANDLER => function ($object, $format, $context) {
+                return $object->getId();
+            },
+        ];
+
+        $encoder = new JsonEncoder();
+        $normalizer = new ObjectNormalizer(null, null, null, null, null, null, $defaultContext);
+        $serializer = new Serializer([$normalizer], [$encoder]);
         $em = $this->getDoctrine()->getManager();
         $jeux = $em->getRepository(Jeu::class)->findAll();
         $jsonContent = $serializer->serialize($jeux, "json");
