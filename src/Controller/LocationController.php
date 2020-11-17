10e682a31ef945abd3d37ad6f64fdbf8bcfc1d1f
JeanVanneste <jeanvanneste@gmail.com>
2020-11-17 12:48:22 +0100

    New locations

--- src/Controller/LocationController.php
+++ src/Controller/LocationController.php
@@ -3,8 +3,12 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+
+use App\Entity\Location;
+use App\Form\CreateLocationType;
 
 class LocationController extends AbstractController
 {
@@ -13,8 +17,40 @@ class LocationController extends AbstractController
      */
     public function index(): Response
     {
+        $this->denyAccessUnlessGranted("ROLE_KEJ");
+
         return $this->render('location/index.html.twig', [
             'controller_name' => 'LocationController',
+        ]);
+    }
+
+    /**
+     * @Route("/location/add", name="location_add")
+     */
+    public function add_location(Request $request)
+    {
+        $this->denyAccessUnlessGranted("ROLE_KEJ");
+
+        $entityManager = $this->getDoctrine()->getManager();
+
+        $location = new Location();
+
+        $form = $this->createForm(CreateLocationType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $location = $form->getData();
+            $location->setOk(false);
+
+
+            $entityManager->persist($location);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('location');
+        }
+
+        return $this->render("location/new.html.twig", [
+            "form" => $form->createView(),
         ]);
     }
 }
---
+++ src/Form/CreateLocationType.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Jeu;
+use App\Entity\Location;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CreateLocationType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('jeu', EntityType::class, [
+                'class' => Jeu::class,
+                'choice_label' => "titre",
+            ])
+            ->add('date_debut', DateType::class)
+            ->add('date_fin', DateType::class)
+            ->add('paye', ChoiceType::class, [
+                'choices' => [
+                    'Oui' => true,
+                    'Non' => false,
+                ],
+            ])
+            ->add('nom', TextType::class)
+            ->add('mail', TextType::class, [
+                'required' => false,
+            ])
+            ->add('phone', TextType::class, [
+                'required' => false,
+            ])
+            ->add('save', SubmitType::class);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Location::class,
+        ]);
+    }
+}
