bea0267f34119df8baa1ae052035ef950134eebc
JeanVanneste <jeanvanneste@gmail.com>
2021-03-23 01:32:06 +0100

    adding location details api support

--- src/Controller/LocationAPIController.php
+++ src/Controller/LocationAPIController.php
@@ -41,4 +41,31 @@ class LocationAPIController extends AbstractController
 
         return $response;
     }
+
+    /**
+     * @Route("api/locations/{id}", name="LocationDetailsAPI")
+     */
+    public function getLocationDetails(int $id): Response
+    {
+        $this->denyAccessUnlessGranted("ROLE_KEJ");
+
+        $defaultContext = [
+            AbstractNormalizer::CIRCULAR_REFERENCE_HANDLER => function ($object, $format, $context) {
+                return $object->getId();
+            },
+        ];
+
+        $encoder = new JsonEncoder();
+        $normalizer = new ObjectNormalizer(null, null, null, null, null, null, $defaultContext);
+        $serializer = new Serializer([$normalizer], [$encoder]);
+        $em = $this->getDoctrine()->getManager();
+        $location = $em->getRepository(Location::class)->find($id);
+        $jsonContent = $serializer->serialize($location, "json");
+
+        $response = new JsonResponse();
+        $response->setContent($jsonContent);
+        $response->headers->set('Access-Control-Allow-Origin', '*');
+
+        return $response;
+    }
 }
